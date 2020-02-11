93737cddbef03709f3d1ca315c8ffdb456c274f7
Student <example@example.com>
2020-02-11 14:03:44 -0800

    everything

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+  return (
+    <div>
+    <h1 className="white-text">Help Queue</h1>
+    <style jsx>{`
+      .white-text {
+        color: white;
+      }
+      `}</style>
+    </div>
+  );
+}
+
+export default Header;
